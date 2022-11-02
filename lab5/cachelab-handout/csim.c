69b101d6e7f35d436c818dbc73f25f135473fcc7
Tesfa Asmara <tasmara@wilkes.cs.hmc.edu>
2022-11-02 12:16:59 -0700

    11/02/22 lecture turned lab session

--- lab5/cachelab-handout/csim.c
+++ lab5/cachelab-handout/csim.c
@@ -1,7 +1,8 @@
 // Cade Kritsch (loginID ckritsch) and Tesfa Asmara (loginID tasmara)
-
+// Remember to uncomment `#-Werror -std=c99` in Makefile by removing the #
 #include "cachelab.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 
 int hit_count;
@@ -10,41 +11,43 @@ int miss_count;
 
 int eviction_count;
 
-struct cache {
-    // 2d array
-    // x - set number
-    // y - block number
-    
-    int* s;
-    int* E;
-    int* b;
-};
 
     // argc is the number of command line arugments. this includes
     // arugment names. argv is the actual inputs, including the names.
 int main(int argc, char** argv) {
-    int s_toRead = ;
-    int E_toRead;
-    int b_toRead;
-    char file_name;
-    char operation;
-    argv[2] //set
-    argv[4] // E
-    argv[6] // b
-    file_name = argv[8] // name of trace file
+
+    // char operation;
+    int s = atoi(argv[2]);
+    int E = atoi(argv[4]);
+    int b = atoi(argv[6]);
+    char* file_name = argv[8];
+
+    printf("%d %d %d %s\n",s,E,b,file_name);
+                        // enough space for an int,
+                        // 2^s*E times over
+   int *cache = malloc(sizeof(int)*(2^s*E));
+
+    printf(*cache);
+    //Todo:
+    // Define a struct for each entry of the cache
+    //      - That struct will contain the valid bit, the tag, the block offset
+    // Eventually incorporate getopt
+    // Parse through the valgrind and keep running sum
+    // Indexing for the cache
     
     // the lines of the file take the format
     // [space]operation address,size
     // we need to read in until EOF, we need to read in the whole line
     // we want to work with, and then we want to condition on the 
     // first char
-    while (scanf("%c", operation, file_name) != 1) {
-        scanf("%c", file_name)
-    }
+    
+    // while (scanf("%c", operation, file_name) != 1) {
+    //     scanf("%c", file_name);
+    // }
 
 
     // cache.s= malloc(s * sizeof s)
-    printSummary(hit_count, miss_count, eviction_count);
+    // printSummary(hit_count, miss_count, eviction_count);
 
     return 0;
 
