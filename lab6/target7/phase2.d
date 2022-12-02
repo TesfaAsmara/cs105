af8246eea74394f5238c15cbb2e2711a50ad7248
Tesfa Asmara <tasmara@wilkes.cs.hmc.edu>
2022-12-01 17:52:34 -0800

    issues with execute permissions on wilkes, going to try different computing cluster

---
+++ lab6/target7/phase2.d
@@ -0,0 +1,9 @@
+
+phase2.o:     file format elf64-x86-64
+
+
+Disassembly of section .text:
+
+0000000000000000 <.text>:
+   0:	48 c7 c7 78 43 27 2d 	mov    $0x2d274378,%rdi
+   7:	c3                   	ret    
